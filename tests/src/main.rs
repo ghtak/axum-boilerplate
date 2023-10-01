7c6bdb5995e98c07802457830cfa58ce37518134
ghtak <gwonhyeong.tak@gmail.com>
2023-10-01 22:11:36 +0900

    feat: future test

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,6 +1,43 @@
 #![allow(unused)]
 
+use std::{future::Future, time::{Instant, Duration}, task::Poll};
+
 use tokio;
+use pin_project;
+
+#[pin_project::pin_project]
+pub struct TimedWrapper<Fut: Future> {
+    start: Option<Instant>,
+    #[pin]
+    inner: Fut,
+}
+
+impl<Fut: Future> TimedWrapper<Fut> {
+    pub fn new(future: Fut) -> Self {
+        Self {
+            start: None,
+            inner: future,
+        }
+    }
+}
+
+impl<Fut: Future> Future for TimedWrapper<Fut> {
+    type Output = (Fut::Output, Duration);
+
+    fn poll(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        let mut this = self.project();
+        let start = this.start.get_or_insert_with(Instant::now);
+        let inner_poll = this.inner.as_mut().poll(cx);
+        let elapsed = start.elapsed();
+        match inner_poll {
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(v) => Poll::Ready((v, elapsed)),
+        }
+    }
+}
 
 #[tokio::test]
 async fn test_main() -> anyhow::Result<()> {
@@ -8,11 +45,12 @@ async fn test_main() -> anyhow::Result<()> {
     hc.do_get("/basic").await?.print().await?;
     hc.do_get("/basic/error").await?.print().await?;
     hc.do_get("/basic/state").await?.print().await?;
+    let async_fn = reqwest::get("http://adamchalmers.com");
+    let timed_async_fn = TimedWrapper::new(async_fn);
+    print!("{:?}", timed_async_fn.await);
     Ok(())  
 }
 
 
 #[tokio::main]
-async fn main(){
-
-}
+async fn main() {}
